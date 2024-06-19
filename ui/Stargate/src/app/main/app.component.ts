017328dc53d6d50b3ff0d2600fe41a19baa6027d
Zack Pudil <zack.pudil@tempworks.com>
2024-06-19 07:38:54 -0500

    Updated styles, adding loading spinners

--- ui/Stargate/src/app/main/app.component.ts
+++ ui/Stargate/src/app/main/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { StargateService } from '../../services/stargate.service';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { PersonAstronaut } from '../../models/PersonAstronaut';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
@@ -9,28 +9,39 @@ import { MatIcon } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddPersonDialogComponent } from '../add-person/add-person.component';
 import { ViewDutyHistoryComponent } from '../view-duty-history/view-duty-history.component';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIcon, MatDialogModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIcon,
+    MatDialogModule,
+    MatProgressSpinner,
+    MatDividerModule
+  ],
   providers: [StargateService]
 })
 export class AppComponent implements OnInit {
-  public personAstronauts$: Observable<PersonAstronaut[]> = new Observable<PersonAstronaut[]>();
 
   readonly dialog = inject(MatDialog);
-
   constructor(private stargateService: StargateService) {}
 
+  personAstronauts$: Observable<PersonAstronaut[]> = new Observable<PersonAstronaut[]>();
+  loading: boolean = true;
 
   ngOnInit(): void {
-    this.personAstronauts$ = this.stargateService.getAstronauts();
+    this.personAstronauts$ = this.stargateService.getAstronauts()
+      .pipe(tap(() => this.loading = false));
   }
 
-  addOrUpdate(isNew: boolean = false, name: string = '') {
+  addOrUpdate(isNew: boolean, name: string = '') {
     const ref = this.dialog.open(AddPersonDialogComponent, {
       data: {
         isNew,
@@ -39,7 +50,9 @@ export class AppComponent implements OnInit {
     });
 
     ref.afterClosed().subscribe(() => {
-      this.personAstronauts$ = this.stargateService.getAstronauts(); 
+      this.loading = true;
+      this.personAstronauts$ = this.stargateService.getAstronauts()
+        .pipe(tap(() => this.loading = false)); 
     });
   }
 
--- ui/Stargate/src/app/view-duty-history/view-duty-history.component.ts
+++ ui/Stargate/src/app/view-duty-history/view-duty-history.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
 import { StargateService } from "../../services/stargate.service";
-import { Observable, map } from "rxjs";
+import { Observable, map, tap } from "rxjs";
 import { AstronautDuty } from "../../models/AstronautDuty";
 import { CommonModule } from "@angular/common";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
     selector: 'app-view-duty-history',
     templateUrl: './view-duty-history.component.html',
     standalone: true,
-    imports: [MatDialogModule, CommonModule, MatListModule, MatIconModule],
+    imports: [MatDialogModule, CommonModule, MatListModule, MatIconModule, MatProgressSpinner],
     providers: [StargateService]
 })
 export class ViewDutyHistoryComponent implements OnInit {
@@ -20,9 +21,13 @@ export class ViewDutyHistoryComponent implements OnInit {
     constructor(private stargateService: StargateService) {}
 
     astronautDuties$: Observable<AstronautDuty[]> = new Observable<AstronautDuty[]>();
+    loading: boolean = true;
 
     ngOnInit(): void {
         this.astronautDuties$ = this.stargateService.getDuties(this.name)
-            .pipe(map(response => response.astronautDuties))
+            .pipe(
+                tap(() => this.loading = false),
+                map(response => response.astronautDuties)
+            )
     }
 }
